b55907da1511114d6b525d1658f9ee5c34c1334b
Daniel-Genkin <genkind@gmail.com>
2021-06-07 15:23:49 -0400

    Proposal 1

---
+++ src/mono/sample/wasm/browser2/main.js
@@ -0,0 +1,73 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Called when the mono-config.js (soon to be mono-config.json via PR#53606) is loaded
+function onConfigLoaded (config) {
+    // we can do some modification to config at runtime here
+    // then return it
+    // i.e. config.newProp = "test new prop";
+
+    return config;
+}
+
+// Called when Emscripten runtime is loaded
+function onRuntimeInitialized () {
+}
+
+// Called when MONO runtime is loaded and ready or if it errors
+function onMonoRuntimeInitialized (error) {
+    if (error) {
+        // mono didn't load correctly
+        test_exit(1); // test runner related
+        throw(error);
+    } else {
+        // mono loaded and we can start doing stuff with it
+        App.init()
+    }
+}
+
+DotNet.onConfigLoaded = onConfigLoaded; // optional
+DotNet.onRuntimeInitialized = onRuntimeInitialized; // optional
+DotNet.onMonoRuntimeInitialized = onMonoRuntimeInitialized; // required as it acts like the entry point into the program
+// They are called in the following order: onConfigLoaded, onRuntimeInitialized, onMonoRuntimeInitialized
+
+
+
+
+// FROM HTML SCRIPT TAG (all js code should be in js files) ////////////////////////////////////////////////////////////////////////////
+var is_testing = false;
+var onLoad = function() {
+    var url = new URL(decodeURI(window.location));
+    let args = url.searchParams.getAll('arg');
+    is_testing = args !== undefined && (args.find(arg => arg == '--testing') !== undefined);
+};
+onLoad();
+
+var test_exit = function(exit_code) {
+    if (!is_testing) {
+        console.log(`test_exit: ${exit_code}`);
+        return;
+    }
+
+    /* Set result in a tests_done element, to be read by xharness */
+    var tests_done_elem = document.createElement("label");
+    tests_done_elem.id = "tests_done";
+    tests_done_elem.innerHTML = exit_code.toString();
+    document.body.appendChild(tests_done_elem);
+
+    console.log(`WASM EXIT ${exit_code}`);
+};
+
+var App = {
+    init: function () {
+        var ret = BINDING.call_static_method("[Wasm.Browser.Sample] Sample.Test:TestMeaning", []);
+        document.getElementById("out").innerHTML = ret;
+
+        if (is_testing)
+        {
+            console.debug(`ret: ${ret}`);
+            let exit_code = ret == 42 ? 0 : 1;
+            test_exit(exit_code);
+        }
+    },
+};
---
+++ src/mono/wasm/runtime/js_support.js
@@ -0,0 +1,84 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+function configLoaded(config) {
+    // In some cases there may be no DotNet object (such as in tests) so we no-op during the callback
+    // Note that DotNet is the new 'Module' object
+    const onConfigLoadedCallback = typeof DotNet !== "undefined" ? DotNet['onConfigLoaded'] : (_) => {};
+    const onMonoRuntimeInitializedCallback = typeof DotNet !== "undefined" ? DotNet['onMonoRuntimeInitialized'] : (_) => {};
+    // Note: onRuntimeInitialized is called by emsdk in another location
+
+    config.loaded_cb = function () {
+        // sends callback so that user knows that mono is loaded
+        onMonoRuntimeInitializedCallback(null);
+    };
+    config.fetch_file_cb = function (asset) {
+        return fetch (asset, { credentials: 'same-origin' });
+    }
+    
+    // sends callback so that user can modify the config as needed before sending it to mono but this is optional
+    // for user to handle
+    config = onConfigLoadedCallback(config) ?? config;
+
+    try
+    {
+        MONO.mono_load_runtime_and_bcl_args (config);
+    } catch (error) {
+        onMonoRuntimeInitializedCallback(error);
+    }
+}
+
+
+
+// MOSTLY COPIED FROM PR#53606 WHICH HAS NOT BEEN MERGED YET /////////////////////////////////////////////////////////
+
+// Loads the config file located in the root of the project
+// Not meant to be used outside of this class (TODO make private to this file when project converted to TS)
+function load_config() {
+    // since this file loads before emsdk we don't have environment vars yet, so we define them locally
+    const ENVIRONMENT_IS_NODE = typeof process === "object";
+    const ENVIRONMENT_IS_WEB = typeof window === "object";
+
+    if (ENVIRONMENT_IS_NODE){
+        try {
+            const config = JSON.parse(require("./mono-config.json"));
+            configLoaded(config);
+        } catch(e) {
+            configLoaded({error: "Error loading mono-config.json file from current directory"});
+        }
+    } else if (ENVIRONMENT_IS_WEB){
+        const xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open("GET", "./mono-config.json", true);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == XMLHttpRequest.DONE) {
+                if (xobj.status === 0 || (xobj.status >= 200 && xobj.status < 400)) {
+                    const config = JSON.parse(xobj.responseText);
+                    configLoaded(config);
+                } else {
+                    // error if the request to load the file was successful but loading failed
+                    configLoaded({error: "Error loading mono-config.json file from current directory"});
+                }
+            }
+        };
+        xobj.onerror = function() {
+            // error if the request failed
+            configLoaded({error: "Error loading mono-config.json file from current directory"});
+        }
+
+        try {
+            xobj.send();
+        } catch(e) {
+            // other kinds of errors
+            configLoaded({error: "Error loading mono-config.json file from current directory"});
+        }
+    } else { // shell or worker
+        try {
+            const config = JSON.parse(read("./mono-config.json")); // read is a v8 debugger command
+            configLoaded(config);
+        } catch(e) {
+            configLoaded({error: "Error loading mono-config.json file from current directory"});
+        }
+    }
+}
+load_config();
